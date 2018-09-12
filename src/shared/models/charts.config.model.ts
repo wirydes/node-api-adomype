144a9f490398d724190700d783ce1495afc6e5a8
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-09-12 01:25:12 -0700

    define charts model

---
+++ src/shared/models/charts.config.model.ts
@@ -0,0 +1,10 @@
+import { ResumeChartsModel } from './resume.charts.model';
+import { MatureLvModel } from './mature.lv.model';
+import { MaturityProfileModel } from './maturity.profile.model';
+
+export class ChartsConfigModel {
+    resumeChart: ResumeChartsModel;
+    matureChart: MatureLvModel;
+    profileChart: MaturityProfileModel;
+
+}
