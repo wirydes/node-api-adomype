2bdd241da9c54d96e78444686ba66b2bc74baaa7
Aurio Alejandro Robles Osuna <alejandro.ro94@gmail.com>
2018-09-13 00:20:48 -0700

    update model for resume chart

--- src/shared/models/resume.charts.model.ts
+++ src/shared/models/resume.charts.model.ts
@@ -1,15 +1,51 @@
 export class ResumeChartsModel {
-    processPlanning: number;
-    formalizationOfProcesses: number;
-    monitoringAndOperationalControl: number;
-    shoppingResults: number;
-    selectionOfNewSuppliers: number;
-    actualSuppliersEvaluation: number;
-    productiveProcessResult: number;
-    deliveryOfValueProcess: number;
-    definitionOfValue: number;
-    customerSatisfaction: number;
-    inventoryControl: number;
-    storage: number;
+    processPlanning: {
+        label: string;
+        value: number;
+    };
+    formalizationOfProcesses: {
+        label: string;
+        value: number;
+    };
+    monitoringAndOperationalControl: {
+        label: string;
+        value: number;
+    };
+    shoppingResults: {
+        label: string;
+        value: number;
+    };
+    selectionOfNewSuppliers: {
+        label: string;
+        value: number;
+    };
+    actualSuppliersEvaluation: {
+        label: string;
+        value: number;
+    };
+    productiveProcessResult: {
+        label: string;
+        value: number;
+    };
+    deliveryOfValueProcess: {
+        label: string;
+        value: number;
+    };
+    definitionOfValue: {
+        label: string;
+        value: number;
+    };
+    customerSatisfaction: {
+        label: string;
+        value: number;
+    };
+    inventoryControl: {
+        label: string;
+        value: number;
+    };
+    storage: {
+        label: string;
+        value: number;
+    };
 
 }
